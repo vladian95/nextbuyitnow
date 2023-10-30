16340d8164216fb0e97dbbdb2c8d20f903803741
vladian95 <kvankvan1995@gmail.com>
2023-10-30 12:33:57 +0300

    products and filters

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,41 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import ListProducts from '../components/products/ListProducts';
+
 const HomePage = () => {
-  return <h1 className="text-1xl font-bold underline">Hello, Next.js!</h1>;
+  const [productsData, setProductsData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`${process.env.API_URL}/api/products`);
+        setProductsData(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Пустой массив зависимостей гарантирует, что эффект выполнится только при монтировании компонента
+
+  return <ListProducts data={productsData} />;
 };
 
 export default HomePage;
+
+// import axios from 'axios';
+// import ListProducts from '../components/products/ListProducts';
+
+// const getProducts = async () => {
+//   const { data } = await axios.get(`${process.env.API_URL}/api/products`);
+//   return data;
+// };
+
+// const HomePage = async () => {
+//   const productsData = await getProducts();
+//   return <ListProducts data={productsData} />;
+// };
+
+// export default HomePage;
